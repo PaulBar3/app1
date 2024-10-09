3cd284a4e401a28675ed7f82c4b775a85ca3dad2
Pavel Baranov <p204368@gmail.com>
2024-10-09 20:31:26 +0300

    main app is ready, pages index , base, about

--- main/views.py
+++ main/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Main page',
-        'content': 'Hello world!',
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME',
     }
 
     return render(request, 'main/index.html', context)
@@ -13,5 +13,24 @@ def index(request):
 
 
 
-def about(request):
-    return HttpResponse('About page')
+def about(request) -> HttpResponse:
+     
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о чем-то '
+    }
+
+    return render(request, 'main/about.html', context)
+
+
+# def contacts(request) -> HttpResponse:
+     
+#     context = {
+#         'title': 'Home - Контакты',
+#         'content': 'Баранавец - Красавец )))',
+#         'content2': 'Дашка - сдобная ЛЯЖКА !!!!',
+        
+#     }
+
+#     return render(request, 'main/contacts.html', context)
