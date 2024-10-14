849cff586b1a336337135e9439d0aceef38d7599
Pavel Baranov <p204368@gmail.com>
2024-10-14 20:44:29 +0300

    added debug -toolbar

--- app/urls.py
+++ app/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import  path, include
 
-
+from app.settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +25,8 @@ urlpatterns = [
     path('catalog/', include('goods.urls', namespace='catalog')),
     
 ]
+
+if DEBUG:  
+    urlpatterns += [
+        path('__debug__/', include("debug_toolbar.urls")),
+    ]
